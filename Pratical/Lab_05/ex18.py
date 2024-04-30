a658cbc389df94ef87d09c4879fdb66b4e8b75a3
João Martins <jmbaltazarmartins@gmail.com>
2024-05-01 00:41:13 +0100

    .

--- Pratical/Lab_05/ex18.py
+++ Pratical/Lab_05/ex18.py
@@ -1,25 +1,12 @@
-def lagrange_interpolation(x, w, mod):
-    total = 0
-    for i in range(len(w)):
-        xi, yi = w[i]
-        def g(i, acc):
-            for j in range(len(w)):
-                if i != j:
-                    xj, yj = w[j]
-                    acc = acc * (x - xj) * mod_inverse(xi - xj, mod) % mod
-            return acc
-        total += g(i, yi)
-        total %= mod
-    return total
-
 def mod_inverse(a, m):
     for x in range(1, m):
         if ((a % m) * (x % m)) % m == 1:
             return x
     return -1
 
-shares = [(7, 15), (12, 16), (17, 9)]
-x = 0
-mod = 23
-secret = lagrange_interpolation(x, shares, mod)
-print(f"O número de Bitcoins que o grupo de amigos comprou na sua juventude é {secret}")
+def calculate_y(x):
+    y = ((22*(x-10)*(x-18)*mod_inverse(10, 23)) + (19*(x-7)*(x-18)*mod_inverse(22, 23)) + (0*(x-7)*(x-10)*mod_inverse(19, 23))) % 23
+    return y
+
+valor = calculate_y(0)
+print(valor)
